72c9f02b873eee7930a19c8ab23f5392ebca5069
Axaxaxa76 <corderomelvin320@gmail.com>
2025-04-11 02:54:04 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/melvin.github.io/',  // Make sure this is correct for your GitHub Pages repo
+  base: '/melvin.github.io/', // Correct base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
